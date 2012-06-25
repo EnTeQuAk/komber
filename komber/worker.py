406bf57f6bba78f69b4e71835d70f125f76cb740
Christopher Grebs <cg@webshox.org>
2012-06-25 14:51:57 +0200

    Make the worker a daemonized thread to ease unittesting

--- komber/worker.py
+++ komber/worker.py
@@ -2,17 +2,20 @@
 
 import os
 import logging
+import threading
 import kombu.utils.debug
 from kombu import Exchange, Queue
 from kombu.mixins import ConsumerMixin
 
 
-class Worker(ConsumerMixin):
+class Worker(threading.Thread, ConsumerMixin):
 
     queues = []
 
     def __init__(self, connection):
         self.connection = connection
+        super(Worker, self).__init__()
+        self.daemon = True
 
     def get_consumers(self, Consumer, channel):
         consumers = []
@@ -24,10 +27,26 @@ class Worker(ConsumerMixin):
             consumers.append(Consumer(queues=[q], callbacks=[handler]))
         return consumers
 
+    def run(self):
+        while not self.should_stop:
+            try:
+                if self.restart_limit.can_consume(1):
+                    for _ in self.consume(limit=None):
+                        pass
+            except self.connection.connection_errors:
+                self.error("Connection to broker lost. "
+                           "Trying to re-establish the connection...")
+
+    def stop(self):
+        self.should_stop = True
+        self.join()
+
 
 def run(conn, debug=False):
     if debug:
         os.environ.update(KOMBU_LOG_CHANNEL="1", KOMBU_LOG_CONNECTION="1")
         kombu.utils.debug.setup_logging(loglevel="DEBUG")
 
-    Worker(conn).run()
+    worker = Worker(conn)
+    worker.start()
+    return worker
